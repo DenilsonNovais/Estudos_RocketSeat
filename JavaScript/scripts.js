b80704f2454fb0840aa8dffe5c3c3a02ef765056
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-12-07 23:35:02 -0300

    Praticando

---
+++ JavaScript/scripts.js
@@ -0,0 +1,45 @@
+//variaveis e tipos de dados
+
+//Declarando uma variavel
+var name
+//Atribuindo valor a variavel ou assignment
+name = "Denilson"
+console.log(name);
+
+//Agrupamento de declarações
+let idade, isHuman 
+idade = 20;
+isHuman = true;
+
+console.log(name, idade, isHuman);
+
+//concatenação de texto com variaveis
+console.log('O ' + name + ' tem ' + idade + ' anos ');
+
+//Interpolando valores com templates literais
+console.log(`O ${name} tem ${idade} anos`);
+
+//object ou objeto
+
+//Criação de um objeto
+const pessoa = {
+    nome: 'João',
+    idade: 20,
+    peso: 88.2,
+    isAdmin: true
+}
+
+//Escrevendo propriedades do objeto
+console.log(pessoa.nome);
+
+//Arrays
+
+//Criando um Array
+const animais = [
+    'Leao',
+    'Macaco',
+    'Gato'
+]
+
+//Acessando valores do Array
+console.log(animais[0]);
