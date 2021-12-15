95947be1c9253c930bb6b6f65e06cb602f6fee70
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-12-15 11:52:41 -0300

    Criando funções

---
+++ JavaScript/Funções/funcoes_1.js
@@ -0,0 +1,18 @@
+//Criar um aplicativo de frases motivacionais
+
+//Declaração da função
+//Function statement
+//Funções servem pra agrupar codigos, reutilização do codigo e dar significado ao bloco de codigo
+function criarFrases() {
+    console.log('Estudar é muito bom');
+    console.log('Paciência e percistência');
+    console.log('Revisão é a mãe do aprendizado');
+}
+
+//Execultar, rodar, chamar, invocar a função
+criarFrases();
+criarFrases();
+criarFrases();
+criarFrases();
+
+console.log('Fim do programa');
