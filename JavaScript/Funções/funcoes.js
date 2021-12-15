d7c8ee28fee31b46703b96e5cbd4d57288ce5dc5
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-12-15 18:33:22 -0300

    Retorno da função

--- JavaScript/Funções/funcoes.js
+++ JavaScript/Funções/funcoes.js
@@ -1,1 +1,17 @@
+//funções dentro de variaveis
+//Funções anonimas ou expression | Não possue nome
+//Parametros da função | Dentro dos parentese
+const somar = function(number1, number2) {
+    let total = number1 + number2
+    return total
+}
+
+let number1 = 34
+let number2 = 25
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`A soma é ${somar(number1, number2)}`)
+
+
 
