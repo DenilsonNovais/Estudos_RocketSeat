c3cf5f5d78da91815e26db20df2cef15e3a14a6a
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-12-18 13:07:09 -0300

    Operador de atribuição e condições

--- JavaScript/dados.js
+++ JavaScript/dados.js
@@ -8,4 +8,56 @@
 
 */
 
+/*
+//Operadores de atribuição (Assignment)
+let x
+
+//Atribuição (Assignment)
+x = 1
+
+//Adicionar atribuição
+//Expressão x = x + 2
+x += 2
+console.log(x);
+
+//Operadores logicos
+let pao = true
+let queijo = true
+
+//And - &&
+//As duas expressões precisam ser verdadeiras
+console.log(pao && queijo)
+
+
+//Or - ||
+//Apenas uma das expressões precisa ser verdadeira
+console.log(pao || queijo)
+
+
+
+//Not - !
+//Inverte as expressões
+console.log(!pao)
+*/
+
+//operações condicionais | se = ? | se não = :
+let pao = false
+let queijo = false
+
+//Se topCafe tiver pao e queijo "café top" se não "café ruim"
+//const topCafe = pao && queijo ? 'Café top' : 'Café ruim'
+
+//Se topCafe tiver pao ou queijo "café top" se não "café ruim"
+const topCafe = pao || queijo ? 'Café top' : 'Café ruim'
+
+console.log(topCafe)
+
+//Maior de 18
+let idade = 16
+const podeDirigir = idade >= 18 ? "Pode dirigir" : "Não pode Dirigir"
+console.log(podeDirigir)
+
+
+
+
 
