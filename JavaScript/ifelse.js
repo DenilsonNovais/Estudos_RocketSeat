5c328d8f33504e0d20426ad85eac51e2d337710d
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-12-18 23:30:59 -0300

    if e else

---
+++ JavaScript/ifelse.js
@@ -0,0 +1,11 @@
+//If e Else
+
+let temperatura = 36
+
+if(temperatura >= 37.5) {
+    console.log('Febre alta')
+}else if(temperatura < 37.5 && temperatura >= 37) {
+    console.log('Febre moderada')
+}else {
+    console.log('Saudavel')
+}
