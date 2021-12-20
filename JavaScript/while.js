f3873b716d183c33048f61e03c680a590e5a4879
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-12-20 15:24:57 -0300

    Estrutura de repetição while

---
+++ JavaScript/while.js
@@ -0,0 +1,11 @@
+//Estrutura de repetição - while | Enquanto
+
+//Inicializador do loop - Uma variavel
+let i = 1000000000000;
+
+//while é usado mais quando não se sabe quando o loop vai parar
+while(i > 10) {
+    console.log(i)
+
+    i /= 35
+}
